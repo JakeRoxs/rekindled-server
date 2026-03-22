7dcbc8752f1f86dd0923c102e47de22ab215b51a
JakeRoxs <jakemorgeson@live.com>
2026-03-21 21:53:44 -0500

    Improve CMake executable path handling in generate_vs2022.bat

--- Tools/generate_vs2022.bat
+++ Tools/generate_vs2022.bat
@@ -1,18 +1,26 @@
 @echo off
 
 SET ScriptPath=%~dp0
-SET RootPath=%ScriptPath%..\
-SET BuildPath=%ScriptPath%..\intermediate\vs2022\
+SET RootPath=%ScriptPath%..
+SET RootPath=%RootPath:~0,-1%
+SET BuildPath=%ScriptPath%..\intermediate\vs2022
 SET CMakeExePath=%ScriptPath%Build\cmake\windows\bin\cmake.exe
 
 if not exist "%CMakeExePath%" (
-    for /f "delims=" %%i in ('where cmake 2^>nul') do set "CMakeExePath=%%i" & goto :foundCMake
+    echo Vendored CMake not found at: %CMakeExePath%, searching PATH...
+    for /f "delims=" %%i in ('where cmake 2^>nul') do (
+        set "CMakeExePath=%%i"
+        echo Found cmake on PATH: %%i
+        goto :foundCMake
+    )
 )
 :foundCMake
 if not exist "%CMakeExePath%" (
     echo ERROR: cmake not found. Install cmake or set PATH to cmake.exe.
     exit /b 1
 )
+
+echo Using CMake executable: %CMakeExePath%
 
 echo Generating %RootPath%
 echo %CMakeExePath% -S %RootPath% -B %BuildPath%
